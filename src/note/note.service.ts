25c40389bea89ce7d65c1aaa2afa2618b974adcd
Abdul-Latif <m3yiel@gmail.com>
2025-10-09 22:48:52 +0300

    added cache to notes

--- src/note/note.service.ts
+++ src/note/note.service.ts
@@ -1,10 +1,12 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { NoteEntity } from 'src/entity/note.entity';
 import { Repository } from 'typeorm';
 import { CreateNoteDto } from 'src/dto/create-note.dto';
 import { UpdateNoteDto } from 'src/dto/update-note.dto';
 import { CategoryEntity } from 'src/entity/category.entity';
+import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
+
 
 
 @Injectable()
@@ -12,10 +14,15 @@ export class NoteService {
     constructor(@InjectRepository(NoteEntity)
     private noteRepo: Repository<NoteEntity>,
         @InjectRepository(CategoryEntity)
-        private categoryRepo: Repository<CategoryEntity>) { }
+        private categoryRepo: Repository<CategoryEntity>,
+        @Inject(CACHE_MANAGER) private cacheManager: Cache) { }
 
-    findAll() {
-        return this.noteRepo.find()
+    async findAll() {
+        const cached = this.cacheManager.get('all-notes')
+        if (cached) return cached;
+
+        const note = this.noteRepo.find()
+        await this.cacheManager.set('all-notes', note, 100000)
     }
 
     findOne(id: number) {
@@ -28,6 +35,7 @@ export class NoteService {
 
         const note = this.noteRepo.create(createNoteDto)
         note.category = category
+        await this.cacheManager.del('all-notes')
         return this.noteRepo.save(note)
 
     }
@@ -42,6 +50,8 @@ export class NoteService {
             where: { category: { id: id } }
         });
 
+        await this.cacheManager.del('all-notes')
+
         if (!note) throw new BadRequestException("No note found in this category");
         category.note = [note];
         Object.assign(note, updateNoteDto)
@@ -49,14 +59,16 @@ export class NoteService {
 
     }
 
-    updateNote(id: number, updateNoteDto: UpdateNoteDto) {
+    async updateNote(id: number, updateNoteDto: UpdateNoteDto) {
         const note = this.noteRepo.findOneBy({ id })
         if (!note) throw new BadRequestException("no note by this id")
+        await this.cacheManager.del('all-notes')
         return this.noteRepo.update(id, updateNoteDto)
     }
 
 
-    remove(id: number) {
+    async remove(id: number) {
+        await this.cacheManager.del('all-notes')
         return this.noteRepo.delete({ id })
     }
 }
